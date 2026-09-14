65da0b3b3da7bac6b61d89db5aed11f9659a8656
Joshua Sam <joshuathomassam@gmail.com>
2026-09-14 23:14:44 +0100

    Bust mobile video player cache

--- app.py
+++ app.py
@@ -284,7 +284,7 @@ def preview():
 
     html = html.replace(
         "</body>",
-        '<script src="/static/mobile-preview.js?v=6" defer></script><script src="/static/video-audio-state.js?v=1" defer></script><script src="/static/lylo-voice.js?v=5" defer></script></body>'
+        '<script src="/static/mobile-preview.js?v=7" defer></script><script src="/static/video-audio-state.js?v=2" defer></script><script src="/static/lylo-voice.js?v=5" defer></script></body>'
     )
 
     return HTMLResponse(content=html)
