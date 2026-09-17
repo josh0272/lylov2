48e51617b5bec0113e9e5baaf9001f0d344a25ed
Joshua Sam <joshuathomassam@gmail.com>
2026-09-17 18:19:31 +0100

    Refresh landing copy cache

--- static/lylo-voice.js
+++ static/lylo-voice.js
@@ -179,7 +179,7 @@
 
 (() => {
   const s = document.createElement('script');
-  s.src = '/static/site-compliance.js?v=1';
+  s.src = '/static/site-compliance.js?v=2';
   s.defer = true;
   document.head.appendChild(s);
 })();
