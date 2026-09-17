c4e36624e5e48da67b47afa05d8e2e6f74c60749
Joshua Sam <joshuathomassam@gmail.com>
2026-09-17 19:26:53 +0100

    Refresh regulatory layout cache

--- static/lylo-voice.js
+++ static/lylo-voice.js
@@ -179,7 +179,7 @@
 
 (() => {
   const s = document.createElement('script');
-  s.src = '/static/site-compliance.js?v=4';
+  s.src = '/static/site-compliance.js?v=5';
   s.defer = true;
   document.head.appendChild(s);
 })();
