b10291738bd962de967060257fffde51d30184b8
Joshua Sam <joshuathomassam@gmail.com>
2026-09-17 22:41:38 +0100

    Bump pilot polish cache version

--- static/lylo-voice.js
+++ static/lylo-voice.js
@@ -186,7 +186,7 @@
 
 (() => {
   const s = document.createElement('script');
-  s.src = '/static/site-pilot-polish.js?v=4';
+  s.src = '/static/site-pilot-polish.js?v=5';
   s.defer = true;
   document.head.appendChild(s);
 })();
