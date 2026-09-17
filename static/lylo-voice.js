09cc29d308be507366a150614142bb191c7dda8a
Joshua Sam <joshuathomassam@gmail.com>
2026-09-17 21:13:45 +0100

    Load exact site text additions

--- static/lylo-voice.js
+++ static/lylo-voice.js
@@ -183,3 +183,10 @@
   s.defer = true;
   document.head.appendChild(s);
 })();
+
+(() => {
+  const s = document.createElement('script');
+  s.src = '/static/site-additions.js?v=1';
+  s.defer = true;
+  document.head.appendChild(s);
+})();
