2942d98295dccbd9907489fa4fa65791296ca108
Joshua Sam <joshuathomassam@gmail.com>
2026-09-17 21:23:17 +0100

    Load pilot polish copy

--- static/live/lylo-voice.js
+++ static/live/lylo-voice.js
@@ -183,3 +183,10 @@
   s.defer = true;
   document.head.appendChild(s);
 })();
+
+(() => {
+  const s = document.createElement('script');
+  s.src = '/static/site-pilot-polish.js?v=1';
+  s.defer = true;
+  document.head.appendChild(s);
+})();
