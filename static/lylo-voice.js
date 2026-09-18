2e40fae47080d64969fa0b2f34dfff9548f3ae27
Joshua Sam <joshuathomassam@gmail.com>
2026-09-18 14:22:13 +0100

    Refresh mission section asset

--- static/lylo-voice.js
+++ static/lylo-voice.js
@@ -186,7 +186,7 @@
 
 (() => {
   const s = document.createElement('script');
-  s.src = '/static/site-pilot-polish.js?v=9';
+  s.src = '/static/site-pilot-polish.js?v=10';
   s.defer = true;
   document.head.appendChild(s);
 })();
