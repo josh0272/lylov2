c2a7f3b4a542e7d58691c897ffba323e55727b10
Joshua Sam <joshuathomassam@gmail.com>
2026-09-18 14:33:32 +0100

    Refresh updated homepage copy assets

--- static/lylo-voice.js
+++ static/lylo-voice.js
@@ -179,14 +179,14 @@
 
 (() => {
   const s = document.createElement('script');
-  s.src = '/static/site-compliance.js?v=6';
+  s.src = '/static/site-compliance.js?v=7';
   s.defer = true;
   document.head.appendChild(s);
 })();
 
 (() => {
   const s = document.createElement('script');
-  s.src = '/static/site-pilot-polish.js?v=11';
+  s.src = '/static/site-pilot-polish.js?v=12';
   s.defer = true;
   document.head.appendChild(s);
 })();
