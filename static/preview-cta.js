b44db15ce434effec78fd97f4be76f93316796b9
Joshua Sam <joshuathomassam@gmail.com>
2026-09-18 15:29:49 +0100

    Serve current homepage HTML: static/preview-cta.js

--- static/preview-cta.js
+++ static/preview-cta.js
@@ -1,6 +1,6 @@
 (() => {
   const script = document.createElement('script');
-  script.src = '/live-assets/preview-cta.js?v=7';
+  script.src = '/live-assets/preview-cta.js?v=8';
   script.defer = true;
   document.head.appendChild(script);
 })();
